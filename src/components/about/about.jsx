80abb53e91106c9a50f959f1380a9fb8cf2e19cf
hiem55 <100002888+hiem55@users.noreply.github.com>
2023-01-21 15:54:45 -0800

    Update base framework for about

---
+++ src/components/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+    return (
+        <section id='about'>
+            <h5>About This Project</h5>
+            <div className="container about_container">
+                <h5> This is the about description</h5>
+
+            </div>
+        </section>
+    )
+}
+
+export default About
