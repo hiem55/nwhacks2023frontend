0bfdfd7ebb3286e80bd83ae756a0726aeab87e0e
hiem55 <100002888+hiem55@users.noreply.github.com>
2023-01-21 15:56:31 -0800

    Add about to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
-// import About from './components/about/about'
+import About from './components/about/about'
 
 const App = () => {
     return (
         <>
             <Header />
+            <About />
             <Footer />
 
         </>
