2b27b5731c96718b67200ea31563cdf0db3b20cb
hiem55 <100002888+hiem55@users.noreply.github.com>
2023-01-21 17:02:15 -0800

    Update footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,12 +3,12 @@ import './footer.css'
 
 const footer = () => {
     return (
-        <header>
+        <footer>
             <div className="container footer_container">
                 <h5> This is the footer</h5>
 
             </div>
-        </header>
+        </footer>
     )
 }
 
