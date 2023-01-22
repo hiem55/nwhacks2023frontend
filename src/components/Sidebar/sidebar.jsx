e5ba0afa64eda5ec27f19e0d072ca0a2e7958ca1
malihawalji <mwalji249@gmail.com>
2023-01-21 17:56:56 -0800

    Stylistic changes to body, about

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -4,7 +4,9 @@ import './sidebar.css'
 const sidebar = () => {
     return (
         <div class="sidebar">
+        <a class="active" href=" ">Home</a>
             <a href="#about">About</a>
+            <a href="#sign up">Sign Up</a>
         </div>
     )
 }
--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -4,9 +4,6 @@ import './body.css'
 const body = () => {
     return (
         <body>  <div class="sidebar">
-                <div>Menu Item 1</div>
-                <div>Menu Item 2</div>
-                <div>Menu Item 3</div>
                 </div>
                 <div class="body-text">
                 </div>
