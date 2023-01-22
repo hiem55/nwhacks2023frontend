81774cc87000603ea73168b26693e8830cc0b4f8
hiem55 <100002888+hiem55@users.noreply.github.com>
2023-01-21 16:33:36 -0800

    Update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './header.css'
+import HeaderSocials from './headersocials'
 
 const header = () => {
     return (
         <header>
             <div className="container header_container">
                 <h5> LIFE 180 This is the header.</h5>
+                <HeaderSocials />
 
             </div>
         </header>
---
+++ src/components/header/headersocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BsGithub } from 'react-icons/bs'
+
+const headersocials = () => {
+    return (
+        <div className='header_socials'>
+            <a href='https://github.com/hiem55/nwhacks2023frontend' targe="_blank"><BsGithub /></a>
+        </div>
+    )
+}
+
+export default headersocials
